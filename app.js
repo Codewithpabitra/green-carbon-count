76382a9d073acb59858112844a7bb4469cf4aa40
Codewithpabitra <pabitra05.maity@gmail.com>
2025-08-14 02:29:11 +0530

    few routes created

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const path = require("path")
 const navRouter = require("./routes/navigation.routes")
 const app = express()
 
 app.set('view engine', "ejs")
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/nav",navRouter)
 
--- routes/navigation.routes.js
+++ routes/navigation.routes.js
@@ -2,8 +2,28 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/",(req,res) => {
-    res.send("hello world")
+router.get("/dashboard",(req,res) => {
+   res.render("dashboard")
+})
+
+router.get("/calculator", (req,res) => {
+    res.render("calculator")
+})
+
+router.get("/aiinsights", (req,res) => {
+    res.send("hello from ai insights")
+})
+
+router.get("/analytics", (req,res) => {
+    res.send("hello from analytics")
+})
+
+router.get("/community", (req,res) => {
+    res.send("hello from community")
+})
+
+router.get("/hub", (req,res) => {
+    res.send("hello from Hub")
 })
 
 module.exports = router
